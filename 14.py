8b3b210c57c1cb8b63c4a52eac415c209ab6bb7b
indorilftw <vgerak@gmail.com>
2013-12-09 18:03:22 +0200

    14 solved, renames

---
+++ 14.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python
+
+maxSeq = 1
+maxNum = 1
+for num in xrange(2,1000000):
+  temp = 0
+  num2 = num
+  while num2 > 1:
+    temp += 1
+    if (num2 % 2 == 0):
+      num2 = num2 / 2
+    else:
+      num2 = 3 * num2 + 1
+  if temp > maxSeq:
+    maxSeq = temp
+    maxNum = num
+print maxNum, maxSeq
