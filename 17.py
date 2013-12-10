181c46fae433ec5696467d2d082038841b441733
indorilftw <vgerak@gmail.com>
2013-12-10 10:26:11 +0200

    22

--- 17.py
+++ 17.py
@@ -1,4 +1,4 @@
-with open('17text.txt', 'r') as myFile:
+with open('files/17text.txt', 'r') as myFile:
     s = myFile.read()
 s2 = "".join(s.split())
 print len(s2)
---
+++ 22.py
@@ -0,0 +1,11 @@
+with open('files/22names.txt', 'r') as myFile:
+  s = myFile.read()
+names = [string[1:-1] for string in s.split(',')]
+names.sort()
+total = 0
+for idx,name in enumerate(names):
+  subtotal = 0
+  for let in name:
+    subtotal += ord(let) - 64
+  total += subtotal * (idx + 1)
+print total
