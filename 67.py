c2e05b196155cb7bfd741db133c3823b1a0580a0
indorilftw <vgerak@gmail.com>
2013-12-10 15:47:48 +0200

    18, 23 & 67

---
+++ 67.py
@@ -0,0 +1,13 @@
+rows = []
+with open("files/67_path.txt", "r") as myFile:
+  for line in myFile: 
+    rows.append([int(i) for i in line.split(" ")])
+ 
+a = []
+for i in range(len(rows)-2, -1 , -1):
+  for j in range(i + 1):
+    a.append((i, j))
+for i,j in a:
+  rows[i][j] +=  max([rows[i+1][j],rows[i+1][j+1]])
+
+print rows[0][0]
