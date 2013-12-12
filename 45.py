94f9b7919cded3c0b54be8c3f7ed00f4ebabe2e1
indorilftw <vgerak@gmail.com>
2013-12-13 00:56:19 +0200

    27, 45

---
+++ 45.py
@@ -0,0 +1,9 @@
+triangle = set()
+pentagonal = set()
+hexagonal = set()
+for i in xrange(100000):
+  triangle.add(i * (i + 1) / 2)
+  pentagonal.add(i * (3 * i - 1) / 2)
+  hexagonal.add(i * (2 * i - 1))
+result = hexagonal & pentagonal & triangle
+print list(result)[3]
