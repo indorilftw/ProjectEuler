91121a14af17e7291f9278ee2a7b921884fbab17
indorilftw <vgerak@gmail.com>
2013-12-13 12:23:28 +0200

    32, 52

---
+++ 32.py
@@ -0,0 +1,7 @@
+pand = set()
+for i in xrange(1,5000):
+  for j in xrange(1,100):
+    s = sorted(str(i) + str(j) + str(i * j))
+    if len(s) == 9 and "".join(s) == '123456789':
+      pand.add(i * j)
+print sum(pand)
